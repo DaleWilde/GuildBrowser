51ab103bbe1c52a5896f26791db1dba2dc669a74
DaleWilde <dale.wilde@landesk.com>
2013-01-18 15:13:51 -0700

    mostly new additions

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Dale Wilde on 1/18/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTest.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTest.h
+//  GuildBrowser
+//
+//  Created by Dale Wilde on 1/17/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTest : SenTestCase
+
+@end
